d2e454918cfd0f50fa0a619363a6fe5c5de159b2
Kurt Pessa <kpessa@gmail.com>
2021-01-30 21:06:53 -0500

    added uv index color functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,6 +47,7 @@ if (testing) {
 }
 
 const fetchCity = cityName => {
+  cityName = toTitleCase(cityName);
   let API_KEY = $('#secret').text().trim();
   var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
 
@@ -54,62 +55,70 @@ const fetchCity = cityName => {
     .then(response => {
       console.log(response);
       response.json().then(data => {
-        if (response.ok) {
+        if (!response.ok) {
+          showFailureMessage();
+        } else if (response.ok) {
           showSuccessMessage();
-        }
-        if (!response.ok) throw "Didn't get response";
-        if (!cities.includes(cityName) && response.ok) {
-          cities.unshift(cityName);
-          localStorage.setItem('cities', JSON.stringify(cities));
-        }
 
-        setSideBarCities();
+          if (!cities.includes(cityName) && response.ok) {
+            cities.unshift(cityName);
+            localStorage.setItem('cities', JSON.stringify(cities));
+          }
 
-        console.log(data);
-        $('#city').text(data.name);
-        $('#cityIcon').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
-        $('#temperature').text(`${kelvinToFahrenheit(data.main.temp).toFixed(1)} F`);
-        $('#humidity').text(`${data.main.humidity}%`);
-        $('#windspeed').text(`${data.wind.speed.toFixed(1)} MPH`);
+          setSideBarCities();
 
-        var lon = data.coord.lon;
-        var lat = data.coord.lat;
-        // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-        var apiURL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${API_KEY}`;
+          console.log(data);
+          $('#city').text(data.name);
+          $('#cityIcon').attr('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
+          $('#temperature').text(`${kelvinToFahrenheit(data.main.temp).toFixed(1)} F`);
+          $('#humidity').text(`${data.main.humidity}%`);
+          $('#windspeed').text(`${data.wind.speed.toFixed(1)} MPH`);
 
-        fetch(apiURL2).then(response2 => {
-          response2.json().then(data2 => {
-            console.log(data2);
-            $('#uvindex').text(`${data2.current.uvi}`); // Set UV-index
+          var lon = data.coord.lon;
+          var lat = data.coord.lat;
 
-            // !  -----------------------
-            // ! Setting 5-Day Forecast
-            // ! ------------------------
-            $('#fiveDay').html('');
-            for (var i = 1; i < 6; i++) {
-              var div = $('<div>');
-              div.addClass('');
-              var card = $('<div>');
-              div.addClass('card bg-primary text-light');
+          // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+          // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+          var apiURL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&appid=${API_KEY}`;
 
-              var h3 = $('<h3>').text(new Date(data2.daily[i].dt * 1000).toLocaleDateString());
-              var img = $('<img>');
-              img.attr('src', `http://openweathermap.org/img/w/${data2.daily[i].weather[0].icon}.png`);
-              var p1 = $('<p>');
-              p1.text(`Temp: ${kelvinToFahrenheit(data2.daily[i].temp.day).toFixed(1)} F`);
-              var p2 = $('<p>');
-              p2.text(`Humidity: ${data2.daily[i].humidity}%`);
-              p2.addClass('mb-0');
+          fetch(apiURL2).then(response2 => {
+            response2.json().then(data2 => {
+              console.log(data2);
+              $('#uvindex').text(`${data2.current.uvi}`); // Set UV-index
+              setUVIndexColor(data2.current.uvi);
 
-              card.append(h3).append(img).append(p1).append(p2);
-              div.append(card);
-              $('#fiveDay').append(div);
-            }
+              // !  -----------------------
+              // ! Setting 5-Day Forecast
+              // ! ------------------------
+              $('#fiveDay').html('');
+              for (var i = 1; i < 6; i++) {
+                var div = $('<div>');
+                div.addClass('');
+                var card = $('<div>');
+                div.addClass('card bg-primary text-light');
+
+                var h3 = $('<h3>').text(new Date(data2.daily[i].dt * 1000).toLocaleDateString());
+                var img = $('<img>');
+                img.attr('src', `http://openweathermap.org/img/w/${data2.daily[i].weather[0].icon}.png`);
+                var p1 = $('<p>');
+                p1.text(`Temp: ${kelvinToFahrenheit(data2.daily[i].temp.day).toFixed(1)} F`);
+                var p2 = $('<p>');
+                p2.text(`Humidity: ${data2.daily[i].humidity}%`);
+                p2.addClass('mb-0');
+
+                card.append(h3).append(img).append(p1).append(p2);
+                div.append(card);
+                $('#fiveDay').append(div);
+              }
+            });
           });
-        });
+        }
       });
     })
-    .catch(error => console.error(error));
+    .catch(error => {
+      showFailureMessage();
+      console.error(error);
+    });
 };
 
 function submitHandler(event) {
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -7,14 +7,43 @@ const getCities = () => {
   return cities;
 };
 
+const toTitleCase = str => {
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
+// !  -----------------------
+// ! Set UV Index color
+// ! ------------------------
+const setUVIndexColor = num => {
+  var color;
+  $('#uvindex').css('color', 'white');
+
+  if (num <= 2) {
+    color = 'green';
+  } else if (num <= 5) {
+    color = 'yellow';
+    $('#uvindex').css('color', 'black');
+  } else if (num <= 7) {
+    color = 'orange';
+  } else if (num <= 10) {
+    color = 'red';
+  } else if (num > 10) {
+    color = 'purple';
+  }
+
+  $('#uvindex').css('background-color', color);
+};
+
 // !  -----------------------
 // ! Setting SideBar Cities
 // ! ------------------------
 const setSideBarCities = () => {
   $('#recentCities').html('');
-  for (var i = 0; i < Math.min(cities.length, 5); i++) {
+  for (var i = 0; i < Math.min(cities.length, 10); i++) {
     var tr = $('<tr>');
-    var td = $('<td>').text(cities[i]);
+    var td = $('<td>').text(toTitleCase(cities[i]));
     tr.append(td);
     $('#recentCities').append(tr);
   }
@@ -30,4 +59,16 @@ const showSuccessMessage = () => {
     $('#alert').fadeOut();
     $('#alert').slideUp(700);
   }, 1500);
+};
+
+// !  -----------------------
+// ! Show Failure Message
+// ! ------------------------
+const showFailureMessage = () => {
+  $('#alert--failure').fadeIn();
+  $('#alert--failure').slideDown(1500);
+  setTimeout(() => {
+    $('#alert--failure').fadeOut();
+    $('#alert--failure').slideUp(1500);
+  }, 3000);
 };
